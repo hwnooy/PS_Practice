045fd59020da1e06c1ab17fec6ea2898555e6072
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-01-13 15:35:53 +0900

    [Bronze II] Title: 수 정렬하기, Time: 100 ms, Memory: 110708 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def bubble(arr):
+    for k in range(len(arr)-1, 0, -1):
+        #print("index K = " , k)
+        for i in range(0, len(arr)-1):
+           # print("index I = ", i)
+            if arr[i]>arr[i+1]:
+                tmp=arr[i]
+                arr[i]=arr[i+1]
+                arr[i+1]=tmp
+    return arr 
+
+arr=[]
+for k in range(n):
+    num=int(input())
+    arr.append(num)
+arr2 = bubble(arr)
+
+for k in arr2:
+    print(k)
+        
