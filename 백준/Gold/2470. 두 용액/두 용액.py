4ce3c538586a62335205d409804d4b4666329766
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-02-15 04:13:08 +0900

    [Gold V] Title: 두 용액, Time: 104 ms, Memory: 124116 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+left, right = 0, n-1
+minSum=float('inf')
+ans = (0, 0)
+while left<right:
+    tempSum = arr[left]+arr[right]
+    
+    if abs(tempSum) < minSum:
+        minSum=abs(tempSum)
+        ans = (arr[left], arr[right])
+        
+    if tempSum>0:
+        right-=1
+    elif tempSum<0:
+        left+=1
+    else:
+        break
+
+print(*ans)
