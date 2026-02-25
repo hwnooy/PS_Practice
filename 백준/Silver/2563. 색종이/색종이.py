05fa8395aa504c67b2ca39cab47c650905ae8f84
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-02-25 23:42:47 +0900

    [Silver V] Title: 색종이, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+paper = [[0]*100 for _ in range(100)]
+n = int(input())
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            paper[i][j]=1
+            
+result = 0
+for row in paper:
+    result+=sum(row)
+    
+print(result)
